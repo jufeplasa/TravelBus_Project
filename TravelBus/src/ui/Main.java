d6eccd406eebf99c45f09461bb2a9195d3e46286
Juan Felipe <jufeplasa@hotmail.com>
2022-05-23 21:06:03 -0500

    I add main class and source package

---
+++ TravelBus/src/ui/Main.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public static void main(String [] arg) {
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
