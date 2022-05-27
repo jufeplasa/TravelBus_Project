588a4386995e3042896ece377c2071a05de8715b
Jorge Eduardo Jojoa Erazo <jorgeeduardo2410@gmail.com>
2022-05-27 08:28:24 -0500

    Welcome.fxml and Routes.fxml classes added

--- TravelBus/src/ui/Main.java
+++ TravelBus/src/ui/Main.java
@@ -1,17 +1,31 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-	public static void main(String [] arg) {
-		
-	}
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+		TravelBusGUI controller = new TravelBusGUI();
+		fxmlLoader.setController(controller);
+		Parent root = fxmlLoader.load();
+		Scene scene = new Scene(root);
+		
+		controller.setMainStage(primaryStage);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Transportadora 1A");
+		primaryStage.show();
+		primaryStage.getIcons().add(new Image("pictures/TravelBus.png"));
+	}
 }
+
---
+++ TravelBus/src/ui/TravelBusGUI.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javafx.stage.Stage;
+
+public class TravelBusGUI {
+private Stage mainStage;
+	
+	public Stage getMainStage() {
+		return mainStage;
+	}
+
+	public void setMainStage(Stage mainStage) {
+		this.mainStage = mainStage;
+	}
+}
