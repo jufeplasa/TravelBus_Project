bd77f3eafa2c7c698dbcaea0e8154f48bf882db0
Jorge Eduardo Jojoa Erazo <jorgeeduardo2410@gmail.com>
2022-05-27 18:16:29 -0500

    Routes.fxml class removed. Added MapRoutes.fxml class.

--- TravelBus/src/ui/TravelBusGUI.java
+++ TravelBus/src/ui/TravelBusGUI.java
@@ -1,9 +1,42 @@
 package ui;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class TravelBusGUI {
-private Stage mainStage;
+	private Stage mainStage;
+
+	@FXML
+	Pane pane;
+	
+	@FXML
+	Circle origin;
+	
+	@FXML
+	Circle destiny;
+	
+	@FXML
+    private ComboBox<?> cmbxChooseOriginDepartment;
+
+    @FXML
+    private ComboBox<?> cmbxChooseOrigin;
+
+    @FXML
+    private ComboBox<?> cmbxChooseDestinationDepartment;
+
+    @FXML
+    private ComboBox<?> cmbxChooseDestination;
+    
 	
 	public Stage getMainStage() {
 		return mainStage;
@@ -12,4 +45,30 @@ private Stage mainStage;
 	public void setMainStage(Stage mainStage) {
 		this.mainStage = mainStage;
 	}
+
+	@FXML
+	public void toRoutesWindow(ActionEvent event) throws IOException {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Routes.fxml"));
+		fxmlLoader.setController(this);
+		Parent root = fxmlLoader.load();
+		Scene scene = new Scene(root);
+
+		mainStage.setScene(scene);
+		mainStage.show();
+	}
+	
+
+	@FXML
+	public void calculateCheapestRoute(ActionEvent event) {
+		
+	}
+
+	
+	
+	@FXML
+	public void calculateFastestRoute(ActionEvent event) {
+		
+	}
+	
 }
