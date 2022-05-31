0308797fa906a40eefb74ab6797990fb763b4e33
Steban Rodriguez <amilcarshm@gmail.com>
2022-06-01 02:53:49 +0800

    add edges method

--- TravelBus/src/model/Ciudades.java
+++ TravelBus/src/model/Ciudades.java
@@ -1,8 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 public class Ciudades {
-	
+	private static Hashtable<Ciudades, Hashtable<Ciudades, String>> hTDistancias;
 	private String ciudad, departamento;
+	private static importData2 data;
+	private static ArrayList<ArrayList<String>> dis =  data.distancias();
+	private static ArrayList<ArrayList<String>> ciu =  data.ciudades();
+	//private ArrayList<ArrayList<String>> cost =  data.costos();
 	
 	public Ciudades(String ciudad, String departamento) {
 			this.ciudad = ciudad;
@@ -24,4 +31,40 @@ public class Ciudades {
 	public void setDepartamento(String departamento) {
 		this.departamento = departamento;
 	}
+	
+	public static void addAristas(ArrayList<ArrayList<String>> ciudad, ArrayList<ArrayList<String>> tipoDeArista) {
+		hTDistancias = new Hashtable<Ciudades, Hashtable<Ciudades, String>>();	
+		Hashtable<Ciudades, String> hTDestino = new Hashtable<Ciudades, String>();
+		
+		//----------------------CIUDADES-------------------------------------------------
+		for (int i = 0; i < ciudad.size(); i++) {
+			ArrayList<String> temp = ciudad.get(i);
+			for (int j = 0; j < temp.size(); j++) {
+				Ciudades c = new Ciudades(temp.get(0), temp.get(1));
+				//-------------------ARISTAS---------------------------------------------
+				for (int k = 0; k < tipoDeArista.size(); k++) {
+					ArrayList<String> temp2 = tipoDeArista.get(i);
+					
+					for (int k2 = 0; k2 < temp2.size(); k2++) {
+						String tipoArista = temp2.get(k2);
+						hTDestino.put(c, tipoArista);
+					}
+				}
+			}
+		}
+		for (int i = 0; i < ciudad.size(); i++) {
+			ArrayList<String> tem = ciudad.get(i);
+			for (int j = 0; j < tem.size(); j++) {
+				Ciudades c = new Ciudades(tem.get(0), tem.get(1));
+				hTDistancias.put(c, hTDestino);
+			}
+		}
+		System.out.println(hTDistancias);
+	}
+
+	public static void main(String[] args) {
+		addAristas(ciu, dis);
+		
+	}
 }
+
--- TravelBus/src/model/importData2.java
+++ TravelBus/src/model/importData2.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class importData2 {
-	private static Hashtable<Ciudades, Double> hTDistancias;
-	private static Hashtable<Ciudades, Double> hTCostos;
+	//private static Hashtable<Ciudades, Double> hTCostos;
 	
 	public static ArrayList<ArrayList<String>> leerArchivo(String nameFile) {
 		ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
@@ -26,39 +25,22 @@ public class importData2 {
 				data.add(temp);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
-		System.out.println(data);
+		//System.out.println(data);
 		return data;
 	}
-	
 	public static ArrayList<ArrayList<String>> distancias() {
-		importData2 fileDistancias = new importData2();
 		ArrayList<ArrayList<String>> dataDistancias = leerArchivo("src/data/distancias.csv");
 		return dataDistancias;
 	}
 	public static ArrayList<ArrayList<String>> costos() {
-		importData2 fileCostos = new importData2();
 		ArrayList<ArrayList<String>> dataCostos = leerArchivo("src/data/costos.csv");
 		return dataCostos;
 	}
 	public static ArrayList<ArrayList<String>> ciudades() {
-		importData2 fileCiudades = new importData2();
 		ArrayList<ArrayList<String>> dataCiudades = leerArchivo("src/data/ciudades.csv");
 		return dataCiudades;
-	}
-	public static Hashtable<Ciudades, Double> add(ArrayList<ArrayList<String>> ciudad, ArrayList<ArrayList<String>> distancia) {
-		hTDistancias = new Hashtable<Ciudades, Double>();
-		for (int i = 0; i < ciudad.size(); i++) {
-			
-			ArrayList<String> temp = ciudad.get(i);
-			
-			for (int j = 0; j < temp.size(); j++) {
-				Ciudades c = new Ciudades(temp.get(j), temp.get(j+1));
-				hTDistancias.put(c, null);
-			}
-		}
-		return hTDistancias;
 	}
 	
 }
